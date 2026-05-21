6b849c204c28c431e24089573be067298d12e370
GopiKrishna-3 <gopikrishna2005gk3@gmail.com>
2026-05-21 09:30:02 +0530

    Update Schema.sql

--- Week 2/Day-5/Schema.sql
+++ Week 2/Day-5/Schema.sql
@@ -1,12 +1,9 @@
-# SCHEMA
+CREATE DATABASE IF NOT EXISTS test;
 
-```sql
+USE test;
+
 -- =========================================
 -- NUMBER MANIPULATION FUNCTIONS IN MYSQL
--- =========================================
-
--- =========================================
--- CREATE TABLE
 -- =========================================
 
 CREATE TABLE employee_salary (
@@ -18,21 +15,13 @@ CREATE TABLE employee_salary (
     experience_years INT
 );
 
--- =========================================
--- INSERT SAMPLE DATA
--- =========================================
-
 INSERT INTO employee_salary VALUES
 (1, 'Karthik', 75000.75, 5000.50, 10.00, 6),
 (2, 'Veena', 65000.40, 4000.25, 8.50, 4),
 (3, 'Ravi', 85000.90, 6000.75, 12.00, 8),
 (4, 'Anil', 70000.10, NULL, 9.00, 5),
 (5, 'Suresh', 60000.55, 3000.30, 7.50, 3);
-```
 
----
-
-```sql
 -- =========================================
 -- REGEX PRACTICE TABLE
 -- =========================================
@@ -45,16 +34,9 @@ CREATE TABLE regex_practice (
     mixed_value VARCHAR(100)
 );
 
--- =========================================
--- INSERT SAMPLE DATA
--- =========================================
-
 INSERT INTO regex_practice VALUES
 (1,'EMP001_IN_91','karthik@gmail.com','+91-9876543210','abc123xyz'),
 (2,'EMP002_US_01','rahul@yahoo.com','+1-7654321098','test45done'),
 (3,'EMP003_UK_44','john@outlook.com','+44-9988776655','run9fast'),
 (4,'EMP004_AU_61','steve@gmail.com','+61-8899776655','go88home'),
 (5,'EMP005_IN_91','arjun@company.in','+91-7788996655','data77lake');
-```
-
----
